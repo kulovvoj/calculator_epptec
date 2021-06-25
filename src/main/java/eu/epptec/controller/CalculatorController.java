fbca315cdf34f1f84d009e82c8387defb1985fa1
kulovvoj <vojtech.kulovany@epptec.eu>
2021-06-25 10:43:22 +0200

    Added exception handling

--- src/main/java/eu/epptec/controller/CalculatorController.java
+++ src/main/java/eu/epptec/controller/CalculatorController.java
@@ -1,16 +1,22 @@
 package eu.epptec.controller;
 
 import eu.epptec.calculator.Calculator;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class CalculatorController {
     @RequestMapping(value = "/{expr}", method = RequestMethod.GET)
     public Double calculate(@PathVariable("expr") String expr) {
-        try {
-            return Calculator.calculate(expr);
-        } catch (IllegalArgumentException e) {
-            return .0;
-        }
+        return Calculator.calculate(expr);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody String
+    badRequest(IllegalArgumentException ex) {
+        return ex.getMessage();
     }
 }
